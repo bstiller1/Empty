4a7256013886a74a2deaeac9298926edadd72723
Blake Stiller <stiller.blake@gmail.com>
2015-10-06 13:21:10 -0400

    Empty C++ Project
    Started in Visual Studio 2010, able to run and work with in Visual
    Studio 2012

---
+++ Empty/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Empty.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
